020f1f2cffb43158a73538d41d86c5835bdeba15
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-06-30 20:57:58 +0100

    Created Demo Repo. Whew! At long last

--- src/Controller/SomeController.php
+++ src/Controller/SomeController.php
@@ -9,17 +9,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SomeController extends AbstractController{
     /**
-     * @Route("/")
+     * @return Response
+     * @Route("/", name="home")
      */
     public function home(){
         return $this-> render('something/index.html.twig');
     }
 
      /**
+      * @return Response
      * @Route("/home")
      */
     public function home2(){
         return $this-> render('something/index.html.twig');
+    }
+    
+     /**
+      * @return Response
+     * @Route("/upload", name="upload")
+     */
+    public function uploadFile(){
+        return $this-> render('something/upload.html.twig');
+    }
+    /**
+      * @return Response
+     * @Route("/purchase", name="purchase")
+     */
+    public function purchaseStamp(){
+        return $this-> render('https://paystack.com/pay/purchase-stamp');
     }
 }
 ?>
